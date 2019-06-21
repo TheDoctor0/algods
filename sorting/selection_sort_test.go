26d016fb1e99d53e1ef4391a8065da16951d2e44
The Doctor <dawid.janik95@gmail.com>
2019-06-21 16:37:41 +0200

    Add selection sort implementation.

---
+++ sorting/selection_sort_test.go
@@ -0,0 +1,17 @@
+package sorting_test
+
+import (
+	"testing"
+
+	"github.com/TheDoctor0/algods/sorting"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSelectionSort(t *testing.T) {
+	data := []int{6, 5, 3, 1, 8, 7, 2, 4}
+	sorted := []int{1, 2, 3, 4, 5, 6, 7, 8}
+
+	sorting.SelectionSort(data)
+
+	assert.Equal(t, data, sorted, "Array should be sorted.")
+}
